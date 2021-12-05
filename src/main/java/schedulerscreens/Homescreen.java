f45e7fc5a357f330043055d837242ae76f0e8b97
Fart972 <lev.komov@gmail.com>
2021-12-05 20:24:24 +0200

    add

---
+++ src/main/java/schedulerscreens/Homescreen.java
@@ -0,0 +1,12 @@
+package schedulerscreens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Homescreen extends BaseScreen{
+    public Homescreen(AppiumDriver<MobileElement> driver) {
+        super(driver);
+    }
+@FindBy
+}
